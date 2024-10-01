d4b3c0d9aca10def004f4ca4200abe0db0db9d21
Muhaimin Al Aziz Hasibuan <muhaiminalaziz06@gmail.com>
2024-10-01 20:47:28 +0700

    perubahan menu hamburger

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
 // Toggle class active
 const navbarNav = document.querySelector('.navbar-nav');
 // ketika hamburger menu di klik
-document.querySelector('#hamburger-menu').onclick = () => {
-  navbarNav.classList.toggle('active');
-};
+document.querySelector('#hamburger-menu').onclick = () => navbarNav.classList.toggle('active');
+document.getElementById('hamburger-menu').addEventListener('click', function (event) {
+  event.preventDefault();
+});
 
 // Klik di luar sidebar untuk menghilangkan nav
 const hamburger = document.querySelector('#hamburger-menu');
